97cd21571d60b36490ca5199647846a2ac4ab8e5
ARisHere <mdahad.mma2017@gmail.com>
2022-09-17 00:58:39 +0600

    Change Maker - The Modern City Originator

---
+++ app/src/main/java/com/team/changemaker/MainActivity.java
@@ -0,0 +1,14 @@
+package com.team.changemaker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
